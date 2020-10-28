d38dd37be0aa10855bb9c9dd0b048f0656215ee3
98973076a <gabriel_coodagolo@hotmail.com>
2020-10-28 20:24:44 -0300

    add prop ao item e criando menu e business

--- src/locadora/entity/Item.java
+++ src/locadora/entity/Item.java
@@ -1,6 +1,61 @@
 package locadora.entity;
 
+import locadora.enums.Tipo;
 
 public class Item {
+    private Long id;
+    private String titulo;
+    private Double valor;
+    private Long estoque;
+    private Tipo tipo;
+    private Long idColecao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Long getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Long estoque) {
+        this.estoque = estoque;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getIdColecao() {
+        return idColecao;
+    }
+
+    public void setIdColecao(Long idColecao) {
+        this.idColecao = idColecao;
+    }
 
 }
